b4602327eaad6950731db71006ef24d55719b35a
Bjamm <blacky.jamm99@gmail.com>
2024-06-19 12:00:00 -0400

    o

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,3 +1,13 @@
 #include "main.h"
 /**
- * print
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c= 'a'; c <= '2'; c++)
+	{
+	_putchar(c);
+	_putchar('\n');
+}
