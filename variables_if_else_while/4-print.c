bb25438de4a20c60fa84d3fb1a9e1c49b579c5c7
Bjamm <blacky.jamm99@gmail.com>
2024-06-26 08:30:52 -0400

    o

---
+++ variables_if_else_while/4-print.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry alphabet except q and e
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i <= 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+		putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
