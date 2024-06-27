0cebc6d269398e01ca4e580c34eaa8078eb9343e
Bjamm <blacky.jamm99@gmail.com>
2024-06-27 17:19:17 -0400

    o

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,11 @@
  */
 int main(void)
 {
-	char (a);
-	int (b);
-	long int (c);
-	long long int (d);
-	float (f);If you are using a linux on Vagrant you might have to install the package libc6-dev-i386 to test the -m32 gcc option
+	printf("Size of a char: %d byte (s) \n", sizeof(char));
+	printf("Size of an int: %d byte (s) \n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	return (0);
 }
